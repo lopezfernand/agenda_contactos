8ae6244317dd587ab229370b889c71e513380b90
Fernando Lopez <l18590236@itsanjuan.edu.mx>
2026-05-06 18:30:18 -0600

    feat: agregar base de datos y red en docker-compose

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///agenda.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://agenda_user:agenda_pass@db:5432/agenda_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
